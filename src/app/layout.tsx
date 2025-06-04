5935081df8ee59361e244ddbdeb92625badc384b
samsoftwaredev <gero@fastmail.com>
2025-06-03 19:33:41 -0500

    update google id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { companyName } from '@/constants';
-import { GoogleTagManager } from '@next/third-parties/google';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: companyName,
@@ -68,6 +68,8 @@ export const metadata: Metadata = {
   },
 };
 
+const GTM_ID = 'G-PVWSHF17ED';
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -75,7 +77,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="G-PVWSHF17ED" />
+      {GTM_ID && (
+        <>
+          <Script
+            id="gtm-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${GTM_ID}');
+              `,
+            }}
+          />
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
+        </>
+      )}
       <body>{children}</body>
     </html>
   );
