52e70705138efe47fcddcd794ce7284840e54c09
Samuel Ruiz <samuelruiz@Samuels-MacBook-Pro.local>
2025-06-04 22:40:57 -0500

    refactor code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,21 +75,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-PVWSHF17ED"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-PVWSHF17ED"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'G-PVWSHF17ED');
         `}
-        </Script>
-        {children}
-      </body>
+      </Script>
+      <body>{children}</body>
     </html>
   );
 }
