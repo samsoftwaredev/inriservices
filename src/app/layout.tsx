f93b87fa344659eac92dd27055be6a858ffd2d5c
Samuel Ruiz <samuelruiz@Samuels-MacBook-Pro.local>
2025-06-04 22:39:50 -0500

    added script tag from google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,8 +68,6 @@ export const metadata: Metadata = {
   },
 };
 
-const GTM_ID = "G-PVWSHF17ED";
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -78,31 +76,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {GTM_ID && (
-          <>
-            <Script
-              id="gtm-init"
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${GTM_ID}');
-              `,
-              }}
-            />
-            <noscript>
-              <iframe
-                src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-                height="0"
-                width="0"
-                style={{ display: "none", visibility: "hidden" }}
-              ></iframe>
-            </noscript>
-          </>
-        )}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-PVWSHF17ED"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-PVWSHF17ED');
+        `}
+        </Script>
         {children}
       </body>
     </html>
